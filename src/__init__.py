ea6fb9c2309e3b794cb60ccae8184e0598eda3f5
Svetlena_k <seva1dan@gmail.com>
2023-03-01 19:45:01 +0200

    TEST 2

--- src/__init__.py
+++ src/__init__.py
@@ -8,24 +8,6 @@ import time
 from src import utils_fs
 from src import copy_to_gcp
 
-#from . import utils_fs
-
-#print("__INIT__ HELLO")
-#__all__ = ["utils_fs"] # if use this => not work IMPORT os ... !!!
-
-sw_break_run = 0
-PROJECT_PATH = utils_fs.Path(__file__).absolute().parent.parent
-path_log = f'{PROJECT_PATH}\log.log'
-print(f'Log File:{path_log}')
-
-''' Create LOGGER '''
-logging.basicConfig(filename = path_log, format='%(asctime)s - %(levelname)s - %(message)s', datefmt = '%m/%d/%Y %I:%M:%S', level=logging.INFO)
-logging.basicConfig(filename = path_log, format='%(asctime)s - %(levelname)s - %(message)s', datefmt = '%m/%d/%Y %I:%M:%S', level=logging.ERROR)
-logging.info('******************* BEGIN _INIT_ *********************')
-#logging.warning('And this, too')
-#logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
-
-
 def get_param_ini(param_name,file_ini):
     dic_ini=[]
     res_nm = ""
@@ -43,14 +25,41 @@ def get_param_ini(param_name,file_ini):
 
     return res_nm
 
+
+#from . import utils_fs
+#print("__INIT__ HELLO")
+#__all__ = ["utils_fs"] # if use this => not work IMPORT os ... !!!
+
+sw_break_run = 0
+PROJECT_PATH = utils_fs.Path(__file__).absolute().parent.parent
+path_log = f'{PROJECT_PATH}\log.log'
+print(f'Log File:{path_log}')
+
+if os.path.isfile(path_log):
+    os.remove(path_log)
+
+''' Create LOGGER '''
+logging.basicConfig(filename = path_log, format='%(asctime)s - %(levelname)s - %(message)s', datefmt = '%m/%d/%Y %I:%M:%S', level=logging.INFO)
+logging.basicConfig(filename = path_log, format='%(asctime)s - %(levelname)s - %(message)s', datefmt = '%m/%d/%Y %I:%M:%S', level=logging.ERROR)
+logging.info('******************* BEGIN _INIT_ *********************')
+#logging.warning('And this, too')
+#logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
+
 #print("HHHH:",os.path.dirname(__file__))
 #print("h1:",Path(__file__).absolute().parent.parent)
 #print("a1:",os.path.abspath(os.curdir))
 
+# from project folder INIT
 FILE_INI = os.path.join(PROJECT_PATH, r"init\init.ini")
 LOCAL_COMP_FILENAME_INI = get_param_ini("LOCAL_COMP_FILENAME_INI", FILE_INI)
 FOLDER_OUT = get_param_ini("FOLDER_OUT", FILE_INI)
+FOLDER_OUT_LOG = get_param_ini("FOLDER_OUT_LOG", FILE_INI)
 LOCAL_COMP_FILENAME_LASTRUN  = get_param_ini("LOCAL_COMP_FILENAME_LASTRUN", FILE_INI)
+HOSTNAME_GCP = get_param_ini("HOSTNAME_GCP", FILE_INI)
+PORT_GCP = get_param_ini("PORT_GCP", FILE_INI)
+USER_GCP = get_param_ini("USER_GCP", FILE_INI)
+PASS_GCP = get_param_ini("PASS_GCP", FILE_INI)
+REMOTE_PATH_GCP = get_param_ini("REMOTE_PATH_GCP", FILE_INI)
 
 ## from local computer
 COMPUTE_NAME_RUN = get_param_ini("COMPUTER_NAME", LOCAL_COMP_FILENAME_INI)
@@ -60,13 +69,22 @@ FOLDERS_LIST_SRC = get_param_ini("FOLDERS_LIST_SRC", LOCAL_COMP_FILENAME_INI)
 if LOCAL_COMP_FILENAME_INI == "ERROR" or FOLDER_OUT == "ERROR" or FILE_INI == "ERROR" or LOCAL_COMP_FILENAME_LASTRUN == "ERROR" or COMPUTE_NAME_RUN == "ERROR" or DISC_DRIVE_NAME == "ERROR" or FOLDERS_LIST_SRC == "ERROR":
     sw_break_run = 1
 
+if not os.path.isdir(FOLDER_OUT):
+    logging.error(f'FOLDER_OUT: {FOLDER_OUT} => NOT EXIST')
+    sw_break_run = 1
+
+if not os.path.isdir(FOLDER_OUT_LOG):
+    logging.error(f'FOLDER_OUT_LOG: {FOLDER_OUT_LOG} => NOT EXIST')
+    sw_break_run = 1
+
+
 now = utils_fs.datetime.now()
 FORMAT_NOW = "%Y-%m-%d %H:%M:%S"
 RUNTIME_NOW = now.strftime("%Y_%m_%d_%H%M%S")
 FILENAME_RUNTIME = f"{COMPUTE_NAME_RUN}_{RUNTIME_NOW}_"
 
 LAST_RUN = get_param_ini("LAST_RUN",LOCAL_COMP_FILENAME_LASTRUN)
-if LAST_RUN == "FILE_NOT_FOUND":
+if LAST_RUN == "ERROR":
     LAST_RUN = "1950-01-01 23:59:59"
     utils_fs.write_last_run(LOCAL_COMP_FILENAME_LASTRUN, LAST_RUN)
 
